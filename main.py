e2baffc7581a2a20168e0e1fba2e6f443d3498ea
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-05-14 00:16:24 -0600

    refactor the function

--- main.py
+++ main.py
@@ -6,25 +6,28 @@ from pymongo.read_concern import ReadConcern
 
 MONGODB_URI = "mongodb+srv://usuario_citas_defunciones:libro_horse_blau_key@cluster0-zaajb.mongodb.net/test"
 
+db = MongoClient(
+    MONGODB_URI,
+    maxPoolSize=50,
+    wtimeout=5000
+    )['defuncionesCR']
 
 def load_data():
-    db = MongoClient(
-        MONGODB_URI,
-        maxPoolSize=50,
-        wtimeout=5000
-        )['defuncionesCR']
 
     citas = []
-    with open("data/def_abril2020_27_30/MOVWEBDEF.txt",'r', encoding='latin_1') as data_file:
+    with open("data/def_febrero2020_24_29/MOVWEBDEF.txt",'r', encoding='latin_1') as data_file:
         for line in data_file:
             citas.append(line[2:14])
-
 
     for numero_cita in citas:
         data = {"identificacion": numero_cita, "tiponacionalidad": 0 }
         response_json = requests.get("https://www.consulta.tse.go.cr/appcdi/home/WSDefuncion", data=data).json()
-        if response_json.get('personaDifunta').get('citaDefuncion') :
-            db.citas_defunciones.insert_one(response_json)
+        try:
+            if response_json.get('personaDifunta').get('citaDefuncion'):
+                    db.citas_defunciones_febrero.insert_one(response_json)
+        except:
+            print(data)
+            print(response_json)
 
 if __name__ == '__main__':
     load_data()
